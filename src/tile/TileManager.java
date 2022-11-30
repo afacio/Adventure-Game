1551733ff3014a03a3e172b2aff8e03b11665b1d
afacio <adas.priv@gmail.com>
2022-11-30 17:40:23 +0100

    tile update

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -29,7 +29,7 @@ public class TileManager {
         tiles = new Tile[50];
         mapTileNumber = new int[gamePanel.MAX_WORLD_COLUMN][gamePanel.MAX_WORLD_ROW];
         getTileImage();
-        loadMap("src/res/maps/worldmap.txt");
+        loadMap("src/res/maps/world02.txt");
     }
 
     private void loadAtlas(){
@@ -37,54 +37,48 @@ public class TileManager {
     }
 
     public void getTileImage(){
-        setup(0, "old/grass", false);
-        setup(1, "old/wall", true);
-        setup(2, "old/water", true);
-        setup(3, "old/earth", false);
-        setup(4, "old/tree", true);
-        setup(5, "old/sand", false);
-        setup(6, "grass00", false);
-        setup(7, "grass00", false);
-        setup(8, "grass00", false);
-        setup(9, "grass00", false);
+        setup(6, "New version/grass00", false);
+        setup(7, "New version/grass00", false);
+        setup(8, "New version/grass00", false);
+        setup(9, "New version/grass00", false);
 
-        setup(10, "grass00", false);
-        setup(11, "grass01", false);
+        setup(10, "New version/grass00", false);
+        setup(11, "New version/grass01", false);
 
-        setup(12, "water00", true);
-        setup(13, "water01", true);
-        setup(14, "water02", true);
-        setup(15, "water03", true);
-        setup(16, "water04", true);
-        setup(17, "water05", true);
-        setup(18, "water06", true);
-        setup(19, "water07", true);
-        setup(20, "water08", true);
-        setup(21, "water09", true);
-        setup(22, "water10", true);
-        setup(23, "water11", true);
-        setup(24, "water12", true);
-        setup(25, "water13", true);
+        setup(12, "New version/water00", true);
+        setup(13, "New version/water01", true);
+        setup(14, "New version/water02", true);
+        setup(15, "New version/water03", true);
+        setup(16, "New version/water04", true);
+        setup(17, "New version/water05", true);
+        setup(18, "New version/water06", true);
+        setup(19, "New version/water07", true);
+        setup(20, "New version/water08", true);
+        setup(21, "New version/water09", true);
+        setup(22, "New version/water10", true);
+        setup(23, "New version/water11", true);
+        setup(24, "New version/water12", true);
+        setup(25, "New version/water13", true);
 
-        setup(26, "road00", false);
-        setup(27, "road01", false);
-        setup(28, "road02", false);
-        setup(29, "road03", false);
-        setup(30, "road04", false);
-        setup(31, "road05", false);
-        setup(32, "road06", false);
-        setup(33, "road07", false);
-        setup(34, "road08", false);
-        setup(35, "road09", false);
-        setup(36, "road10", false);
-        setup(37, "road11", false);
-        setup(38, "road12", false);
+        setup(26, "New version/road00", false);
+        setup(27, "New version/road01", false);
+        setup(28, "New version/road02", false);
+        setup(29, "New version/road03", false);
+        setup(30, "New version/road04", false);
+        setup(31, "New version/road05", false);
+        setup(32, "New version/road06", false);
+        setup(33, "New version/road07", false);
+        setup(34, "New version/road08", false);
+        setup(35, "New version/road09", false);
+        setup(36, "New version/road10", false);
+        setup(37, "New version/road11", false);
+        setup(38, "New version/road12", false);
 
-        setup(39, "earth", false);
+        setup(39, "New version/earth", false);
 
-        setup(40, "wall", true);
+        setup(40, "New version/wall", true);
 
-        setup(41, "tree", true);
+        setup(41, "New version/tree", true);
     }
 
     public void setup(int index, String imageName, boolean collision){
